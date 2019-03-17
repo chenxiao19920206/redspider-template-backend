2df0abb01cfcb154176bf06bb6a97c0aeeb7ade4
git <chenxiao19920206@163.com>
2019-03-17 16:03:50 +0800

    init

---
+++ src/main/java/group/redspider/template/controller/BaseController.java
@@ -0,0 +1,14 @@
+package group.redspider.template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello!";
+    }
+}
